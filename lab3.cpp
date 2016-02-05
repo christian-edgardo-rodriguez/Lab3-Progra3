f4b834d572d1bcd0d5904f63b7b903ac1ea817f6
christian-edgardo-rodriguez <christian.rodriguez@unitec.edu>
2016-02-05 14:03:07 -0600

    hice el menu y algunas validaciones

---
+++ lab3.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <cstdlib>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int menu();
+void mayorMenor(int , int);
+void printArray(int [], int);
+
+int main(int argc, char const *argv[]){
+	int size=10, array[size], opcion=menu(), vecesJuego=1, currentTry, numIntentos;
+	srand(time(NULL));
+	int numRand=rand()%1001-500;
+	if (opcion==1){
+		while(vecesJuego<=10){
+			for(int i=0;i<1001;i++){
+			cout<<"Intento #"<<i+1<<": ";
+			cin>> currentTry;
+			numIntentos++;
+			if(currentTry==numRand){
+				cout<<"Este es el numero!!!"<<endl;
+				vecesJuego++;
+				array[vecesJuego-1]=numIntentos;
+				numIntentos=0;
+				cout<<endl;
+				break;					
+			}else
+				mayorMenor(currentTry,numRand); 
+			}
+		}
+		cout<<"-----------------------------"<<endl;
+		printArray(array, size);
+	}else if (opcion==2){
+		
+	}else
+		cout<<"Opcion Invalida"<<endl;
+	return 0;
+}
+int menu(){
+	int opc;
+	cout<<"1. Juego de loteria"<<endl;
+	cout<<"2. Juego de batalla"<<endl;
+	cout<<"Escoja su opcion: ";
+	cin>>opc;
+	if (opc<1&&opc>2){
+		return 0;
+	}else{
+		return opc;
+	}
+}
+void mayorMenor(int currentTry,int numRand){
+	if (currentTry<numRand){
+		cout<<"Ingrese un numero mayor"<<endl;
+	}else if (currentTry>numRand){
+		cout<<"Ingrese un numero menor"<<endl;
+	}
+}
+void printArray(int array[], int size){
+	for (int i = 0; i < size; ++i){
+		cout<<array[i]<<endl;
+	}
+}
