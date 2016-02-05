da1d1a5a48cc4d56c2e44d19153ed0281ee19738
christian-edgardo-rodriguez <christian.rodriguez@unitec.edu>
2016-02-05 17:28:03 -0600

    hice el ejercicio 2

--- lab3.cpp
+++ lab3.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <ctime>
+#include <iomanip>
 #include <cstdlib>
 
 using std::cout;
 using std::cin;
 using std::endl;
+using std::setw;
 
 int menu();
 void mayorMenor(int , int);
@@ -17,7 +20,6 @@ int main(int argc, char const *argv[]){
 			srand(time(NULL));
 			int numRand=rand()%1001-500;
 			do{
-				cout<<numRand<<endl;
 				cout<<"Ingrese el numero: ";
 				cin>> currentTry;
 				numIntentos++;
@@ -47,7 +49,73 @@ int main(int argc, char const *argv[]){
 		cout<<endl;
 		opcion=menu();
 	}else if (opcion==2){
-		
+		srand(time(NULL));
+		const int row=4;
+		const int col=3;
+		int matriz[row][col], sizeJug1=3, sizeJug2=3;
+		int jugador1[sizeJug1], jugador2[sizeJug2];
+		int atkJug1, defJug1, velJug1, atkJug2, defJug2, velJug2, ans=1;
+		while(ans==1){
+			for (int i = 0; i < row; ++i){
+				for (int j = 0; j < col; ++j){
+					int randAtk=rand()%30+85;
+					int randDef=rand()%25+50;
+					int randVel=rand()%50+150;
+					if (j==0){
+						matriz[i][0]=randAtk;
+					}else if (j==1){
+						matriz[i][1]=randDef;
+					}else{
+						matriz[i][2]=randVel;
+					}
+				}
+			}
+			for (int i = 0; i < row; ++i){
+				for (int j = 0; j < col; ++j){
+					cout<<setw(8)<<matriz[i][j];
+				}
+				cout<<endl;
+			}
+			cout<<endl;
+			for (int i = 0; i < sizeJug1; ++i){
+				int randJug1=rand()%4;
+				if (i==0){
+					atkJug1=matriz[randJug1][0];
+				}else if (i==1){
+					defJug1=matriz[randJug1][1];
+				}else{
+					velJug1=matriz[randJug1][2];
+				}
+			}
+			for (int i = 0; i < sizeJug2; ++i){
+				int randJug2=rand()%4;
+				if (i==0){
+					atkJug2=matriz[randJug2][0];
+				}else if (i==1){
+					defJug2=matriz[randJug2][1];
+				}else{
+					velJug2=matriz[randJug2][2];
+				}
+			}
+			int resultadoJug1=atkJug1-defJug2;
+			int resultadoJug2=atkJug2-defJug1;
+			if (resultadoJug1>resultadoJug2){
+				cout<<"Jugador 1 gano por "<<resultadoJug1-resultadoJug2<<" puntos, no gano por velocidad. "<<endl;
+			}else if (resultadoJug2>resultadoJug1){
+				cout<<"Jugador 2 gano por "<<resultadoJug2-resultadoJug1<<" puntos, no gano por velocidad. "<<endl;
+			}else if (resultadoJug1=resultadoJug2){
+				if (velJug1>velJug2){
+					cout<<"Jugador 1 gano por "<<velJug1-velJug2<<" puntos, gano por velocidad. "<<endl;	
+				}else if (velJug2>velJug1){
+					cout<<"Jugador 2 gano por "<<velJug2-velJug1<<" puntos, gano por velocidad. "<<endl;	
+				}else{
+					cout<<"EMPATE TOTAL"<<endl;
+				}
+			}
+			cout<<"Quiere seguir jugando? [1/2]: ";
+			cin>>ans;
+		}
+		int opcion=menu();
 	}else
 		cout<<"Opcion Invalida"<<endl;
 	return 0;
@@ -74,5 +142,10 @@ void mayorMenor(int currentTry,int numRand){
 void printArray(int array[], int size){
 	for (int i = 0; i < size; ++i){
 		cout<<array[i]<<endl;
+	}
+}
+void printMatriz(int array[], int size){
+	for (int i = 0; i < size; ++i){
+		cout<<setw(8)<<array[i]<<endl;
 	}
 }
